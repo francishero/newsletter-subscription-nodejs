b9168a8f78344f2a0b3f3ad0fa7b7802465c3af4
Francis Miyoba <francismiyoba@gmail.com>
2017-06-11 17:14:57 +0800

    EmailChimp integration done

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const app=express()
 
-app.listen(3000,()=>console.log('Server running'))
+app.listen(process.env.PORT || 3001,()=>console.log('Server running'))
 
 //middleware
 app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.post('/',(req,res)=>{
-	res.end('Hello world')
+	addEmailToMailChimp(req.body.email)
+	//console.log(req.body.email)
+	res.end('Success!!')
 })
+
+function addEmailToMailChimp(email)
+{
+	var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://us16.api.mailchimp.com/3.0/lists/dccd34011f/members',
+  headers: 
+   { 'postman-token': 'f394040c-bad0-d474-3708-1ff8c909ce04',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json',
+     authorization: 'Basic YW55c3RyaW5nOjQ1ZTFiMjIxZGNkOTc2NDgwOWQ5ZTk4MDQzZGM3ZmZmLXVzMTY=' },
+  body: { email_address: email, status: 'subscribed' },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,22 @@
-console.log('Working')
+
+const $=require('jquery')
+
+
+
+$('form').submit((event)=>{
+
+	const userEmail=$('#email').val()
+	console.log(userEmail)
+	event.preventDefault()
+
+	$.ajax({
+	url:'/',
+	type:'POST',
+	data:{
+		email:userEmail 
+	},
+	success:(response)=>console.log(response)
+})
+
+})
+
